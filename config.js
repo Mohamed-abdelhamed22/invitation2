aac050f0b4dbdcd383b9cf5ad285661a09c15a38
Mohamed-abdelhamed22 <mohamed.abdelhamed6112k@gmail.com>
2026-09-04 14:47:47 +0300

    .

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const INVITATION = {
   couple: { bride: 'Hala', groom: 'Mohammed' },
-  date: '2026-09-17T20:00:00+03:00',
+  date: '2026-09-17T19:30:00+03:00',
   dateFrench: '17 September 2026',
-  timeFrench: '08:00PM',
+  timeFrench: '07:30PM',
   venue: 'مسجد العلي العظيم',
   city: 'Almaza Cairo, Egypt',
   mapsUrl: 'https://maps.app.goo.gl/63B6a7LD2CrKDjRz7',
@@ -17,7 +17,7 @@ const INVITATION = {
     { time: '16:00', title: 'Welcome', text: 'Accueil des invités' },
     { time: '17:00', title: 'Ceremony', text: 'La cérémonie commence' },
     { time: '18:30', title: 'Dinner', text: 'Dîner et célébration' },
-    { time: '20:00', title: 'Party', text: 'Musique et danse' }
+    { time: '19:30', title: 'Party', text: 'Musique et danse' }
   ],
   gallery: ['assets/IMG-20241213-WA0011.jpg','assets/IMG-20250314-WA0041.jpg','assets/IMG-20250413-WA0034.jpg','assets/IMG-20250701-WA0047.jpg','assets/IMG_4517.jpg','assets/IMG_8324.jpg','assets/IMG_0374.jpg','assets/IMG_0551.jpg','assets/IMG_3009.jpg','assets/IMG_4833.jpg','assets/IMG_4970.jpg','assets/IMG_5067.jpg','assets/IMG_8757.jpg','assets/IMG_8789.jpg'],
   splash: {
